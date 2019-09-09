88120ae1eea4a7cd07e65369eb0d79619acaa5b5
Caio Almeida <caioalmeidanaweb@gmail.com>
2019-09-09 13:53:48 -0400

    finalização do curso

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {bindActionCreators} from 'redux';
+import {clickHelloAction} from './actions/index';
+import  { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  render(){
+    const {text} = this.state;
+    const {msg, clickHelloAction} = this.props;
+
+    return (
+      <div className="App">
+        <input type="text" onChange={this.handleInputChange} />
+       <button type="button" onClick={ () => clickHelloAction(text)}>Click to dispath</button>
+       <h1>{msg}</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (store) => {
+  return{
+    msg: store.clickReducer.msg
+  }
+}
+
+const mapDispatchToProps = (dispatch) => 
+ bindActionCreators({clickHelloAction},dispatch);
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
